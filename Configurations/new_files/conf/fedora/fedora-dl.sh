a0f109e8460459c41328b12c3811fc5c751e5e6c
Milad Khanlou <104443908+miladkhanlou@users.noreply.github.com>
2024-05-21 10:54:56 -0500

    Delete Configurations/new_files directory

--- Configurations/new_files/conf/fedora/fedora-dl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo wget -O fcrepo.war https://github.com/fcrepo/fcrepo/releases/download/fcrepo-6.4.1/fcrepo-webapp-6.4.1.warsudo mv fcrepo.war /opt/tomcat/webapps
-sudo wget -O fcrepo.war https://github.com/fcrepo/fcrepo/releases/download/fcrepo-6.5.0/fcrepo-webapp-6.5.0.war
-sudo mv fcrepo.war /opt/tomcat/webapps
-sudo chown tomcat:tomcat /opt/tomcat/webapps/fcrepo.war
-sudo systemctl start tomcat
-
